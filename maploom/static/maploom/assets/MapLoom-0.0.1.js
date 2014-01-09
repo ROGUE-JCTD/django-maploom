4af3cc99a3b27f2bc17790386b61ca7df8d2bfeb
roguejenkins <roguejenkins@lmnsolutions.com>
2014-01-09 11:36:19 -0500

    jenkins job django-maploom: use latest maploom to build maploom django wrapper.

--- maploom/static/maploom/assets/MapLoom-0.0.1.js
+++ maploom/static/maploom/assets/MapLoom-0.0.1.js
@@ -64636,7 +64636,8 @@ ol.tilegrid.XYZOptions;
     '$location',
     '$translate',
     'mapService',
-    function AppCtrl($scope, $window, $location, $translate, mapService) {
+    'debugService',
+    function AppCtrl($scope, $window, $location, $translate, mapService, debugService) {
       console.log('---- ngBoilerplate.controller.');
       $scope.$on('$stateChangeSuccess', function (event, toState) {
         if (angular.isDefined(toState.data.pageTitle)) {
@@ -64646,6 +64647,12 @@ ol.tilegrid.XYZOptions;
       $scope.mapService = mapService;
     }
   ]);
+  module.provider('debugService', function () {
+    this.$get = function () {
+      return this;
+    };
+    this.showDebugButtons = false;
+  });
   module.config([
     '$translateProvider',
     function ($translateProvider) {
@@ -69373,15 +69380,17 @@ var GeoGitLogOptions = function () {
     '$translate',
     'refreshService',
     'testService',
-    function ($rootScope, notificationService, diffService, mapService, dialogService, $timeout, pulldownService, $translate, refreshService, testService) {
+    'debugService',
+    function ($rootScope, notificationService, diffService, mapService, dialogService, $timeout, pulldownService, $translate, refreshService, testService, debugService) {
       return {
         restrict: 'C',
         replace: true,
         scope: true,
-        template: '<div class="panel flat">' + '  <div class="btn-group">' + '    <button type="button" ng-click="switchCoords()"' + '      class="btn btn-default">Switch Coords</button>' + '    <button type="button" ng-click="addNotification()"' + '      class="btn btn-default">Post Notification</button>' + '    <button type="button" ng-click="performDiff()"' + '      class="btn btn-default">Perform Diff</button>' + '    <button type="button" ng-click="clearDiff()"' + '      class="btn btn-default">Clear Diff</button>' + '    <button type="button" ng-click="mapService.activateDragZoom()"' + '      class="btn btn-default">Drag Zoom</button>' + '    <button type="button" ng-click="addModal()"' + '      class="btn btn-default">Modal</button>' + '    <button type="button" ng-click="localize()"' + '      class="btn btn-default">Switch Language</button>' + '    <button type="button" ng-click="refreshService.refreshLayers()"' + '      class="btn btn-default">Refresh Layers</button>' + '    <button type="button" ng-click="testStart()"' + '      class="btn btn-default">Start Test</button>' + '    <button type="button" ng-click="testStop()"' + '      class="btn btn-default">Stop Test</button>' + '  </div>' + '</div>',
+        template: '<div class="panel flat">' + '  <div class="btn-group" ng-show="debugService.showDebugButtons">' + '    <button type="button" ng-click="switchCoords()"' + '      class="btn btn-default">Switch Coords</button>' + '    <button type="button" ng-click="addNotification()"' + '      class="btn btn-default">Post Notification</button>' + '    <button type="button" ng-click="performDiff()"' + '      class="btn btn-default">Perform Diff</button>' + '    <button type="button" ng-click="clearDiff()"' + '      class="btn btn-default">Clear Diff</button>' + '    <button type="button" ng-click="mapService.activateDragZoom()"' + '      class="btn btn-default">Drag Zoom</button>' + '    <button type="button" ng-click="addModal()"' + '      class="btn btn-default">Modal</button>' + '    <button type="button" ng-click="localize()"' + '      class="btn btn-default">Switch Language</button>' + '    <button type="button" ng-click="refreshService.refreshLayers()"' + '      class="btn btn-default">Refresh Layers</button>' + '    <button type="button" ng-click="testStart()"' + '      class="btn btn-default">Start Test</button>' + '    <button type="button" ng-click="testStop()"' + '      class="btn btn-default">Stop Test</button>' + '  </div>' + '</div>',
         link: function (scope) {
           scope.mapService = mapService;
           scope.refreshService = refreshService;
+          scope.debugService = debugService;
           function addNotification() {
             notificationService.addNotification({
               text: '1 Added, 3 Modified, 1 Deleted',
