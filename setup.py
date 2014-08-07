a770845f2f84424d2eb2eefe88e0186fa218041b
roguejenkins <roguejenkins@lmnsolutions.com>
2014-08-07 14:05:50 -0400

    jenkins job django-maploom: use latest maploom to build maploom django wrapper.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 
 setup(
     name='django-maploom',
-    version='0.0.1@2014-08-06.11:08:12.a7b95ba8fc',
+    version='0.0.1@2014-08-07.14:05:50.a7b95ba8fc',
     author='LMN Solutions',
     author_email='rogue@lmnsolutions.com',
     url='https://github.com/ROGUE-JCTD/django-maploom',
