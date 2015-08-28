0e8c31642853c10733cf3567c90879d7bc10f510
Syrus Mesdaghi <syrusm@gmail.com>
2015-08-27 23:01:22 -0400

    new release to include layer order fix.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 
 setup(
     name='django-maploom',
-    version='0.0.1@2015-08-27.22:57:01.dc3c62d106',
+    version='1.2.7',
     author='LMN Solutions',
     author_email='geoshape.org@gmail.com',
     url='https://github.com/ROGUE-JCTD/django-maploom',
