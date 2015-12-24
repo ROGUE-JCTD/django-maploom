9d73f440c429f9be605b3948573ad13b04328872
Syrus Mesdaghi <syrusm@prominentedge.com>
2015-12-24 09:10:43 -0500

    updated build script to work for "dev" and "django"

--- deploy/build.sh
+++ deploy/build.sh
@@ -1,14 +1,38 @@
 #!/bin/bash
-## this script is used by jenkins to update the django-maploom project after building latest maploom
+## this script is used by jenkins or a developer with local build of MapLoom
+## to update the django-maploom project after building latest maploom
+
 # exit if anything returns failure
 set -e
 
-pwd
+echo 'current directory:' pwd
+
+
+if [ "$1" = "jenkins" ]; then
+  JENKINS_MODE=true
+elif [ "$1" = "dev" ]; then
+  JENKINS_MODE=false
+else
+    echo "usage: "
+    echo "       deploy jenkins"
+    echo "           assumes maploom is found and already built at ../../maploom/lastSuccessful/archive"
+    echo "       deploy dev"
+    echo "           assumes maploom is found and already built at ../MapLoom"
+    exit 1
+fi
 
 git checkout master
 
 # path to the maploom build that will be used
-MAPLOOM_PATH=../../maploom/lastSuccessful/archive
+if [ $JENKINS_MODE == true ]; then
+  MAPLOOM_PATH=../../maploom/lastSuccessful/archive
+  MAPLOOM_REPO=../../maploom/workspace
+else
+  MAPLOOM_PATH=../MapLoom
+  MAPLOOM_REPO=$MAPLOOM_PATH
+fi
+
+echo "Using maploom build at: "$MAPLOOM_PATH
 
 # get the new index.html file and use it to make the partial
 sed -n '/body class="maploom-body">/,/body>/p' $MAPLOOM_PATH/bin/index.html > index_body.html
@@ -33,16 +57,26 @@ mv _maploom_map.html maploom/templates/maploom
 # a version number which we can set in setup.py
 VER_DATE=`date +%Y-%m-%d.%H:%M:%S`
 pushd .
-cd ../../maploom/workspace
+cd $MAPLOOM_REPO
 VER_SHA1=`git log --format="%H" | head -n1 | cut -c 1-10`
 popd
 VER=$VER_DATE.$VER_SHA1
-sed -i "s|^    version=.*|    version='0.0.1@"$VER_DATE.$VER_SHA1"',|" ./setup.py
+#echo "s|^    version=.*|    version='0.0.1@"$VER_DATE.$VER_SHA1"',|" ./setup.py
+sed -ie "s|^    version=.*|    version='0.0.1@"$VER_DATE.$VER_SHA1"',|" ./setup.py
 
 # if git status doesn't have 'nothing' (to commit) in it, go ahead and commit
 # for this to work you can, 1) cd ~ 2) ssh-keygen -t rsa (Press enter for all values) 3) add pub key to the repo's deploy keys on github. 
 if [[ $(git status) != *nothing* ]]; then
   git add .
-  git commit -m "jenkins job django-maploom: use latest maploom to build maploom django wrapper."
+
+  # path to the maploom build that will be used
+  if [ $JENKINS_MODE == true ]; then
+    COMMIT_MSG="jenkins job django-maploom: use latest maploom to build maploom django wrapper."
+  else
+    COMMIT_MSG="developer deployed using deploy script with dev flag."
+  fi
+  git commit -m "$COMMIT_MSG"
   git push origin master
 fi
+
+echo 'done!'
